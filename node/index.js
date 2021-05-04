d3177e0d0797f6efee9b57f94688fa0f86cdd375
Caio Queiroz <caio.cesar.g.queiroz@accenture.com>
2021-05-04 16:20:11 -0300

    Criacao da tabela people ao acessar servico node

--- node/index.js
+++ node/index.js
@@ -9,14 +9,18 @@ const config = {
 };
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
+const createTableSql = `CREATE TABLE IF NOT EXISTS people (id int auto_increment primary key, name varchar(255))`;
+connection.query(createTableSql);
+const sql = `INSERT INTO people(name) values('Caio Queiroz')`
+connection.query(sql);
 
-app.get('/', async (req,res) => {
-  const sql = `INSERT INTO people(name) values('Caio Queiroz')`
-  await connection.query(sql)
-  // const sqlSelect = `SELECT * FROM people;`;
-  // const rows = connection.query(sqlSelect);
-  // connection.end()
-  res.send(`<h1>Full Cycle</h1><p></p>`);
+app.get('/', (req,res) => {
+  res.send(`<h1>Full Cycle</h1>`);
+
+  const sqlSelect = `SELECT name FROM people;`;
+  connection.query(sqlSelect, (err, rows) => {
+    console.log('People:', rows);
+  })
 })
 
 app.listen(port, ()=> {
